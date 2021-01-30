5ae7f7b2e61648bc444f65f8c1271473c18d81d8
russiancplusplus <gatsbynalda@gmail.com>
2021-01-30 12:13:43 +0300

    Simple get requests have been added

--- src/ru/gatsbyx/springmvcapp/controllers/FirstController.java
+++ src/ru/gatsbyx/springmvcapp/controllers/FirstController.java
@@ -1,18 +1,47 @@
 package ru.gatsbyx.springmvcapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
 	@GetMapping("/hello")
-	public String helloPage() {
+	public String helloPage(@RequestParam(value = "name", required = false) String name, 
+							@RequestParam(value = "surname", required = false) String surname,
+							Model model) {
+		model.addAttribute("message", "Hello, " + name + " " + surname);
 		return "first/hello";
 	}
 	
 	@GetMapping("/goodbye")
 	public String goodByePage() {
 		return "first/goodbye";
+	}
+	
+	@GetMapping("/calc")
+	public String calculator(@RequestParam("a") double a, 
+			@RequestParam("b") double b, 
+			@RequestParam("operation") String operation, Model model) {
+		
+		double result = 0.0;
+		switch(operation) {
+			case "multiplication":
+				result = a * b;
+				break;
+			case "division":
+					result = a / b;
+					break;
+			case "subtraction":
+				result = a - b;
+				break;
+			case "addition":
+				result = a + b;
+				break;
+		}
+		model.addAttribute("result", result);
+		return "first/calculator";
 	}
 }
